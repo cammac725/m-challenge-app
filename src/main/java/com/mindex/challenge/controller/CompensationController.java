836da31afbb2e2362ca0aff3c2d901a45d8ab28a
cammac <camm@rocktechcomputer.com>
2021-03-26 20:06:56 -0700

    trying to solve bean error

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -3,7 +3,15 @@ package com.mindex.challenge.controller;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.service.CompensationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class CompensationController {
@@ -11,13 +19,27 @@ public class CompensationController {
     @Autowired
     private CompensationService compensationService;
 
-    @PostMapping("/compensation")
-    public Compensation create(@RequestBody Compensation compensation) {
-        return compensationService.create(compensation);
+    @PostMapping(value = "/compensation", consumes = "application/json")
+    public ResponseEntity<?> create(
+            @Valid
+            @RequestBody Compensation newCompensation) throws URISyntaxException {
+        newCompensation.setEmployee("987987987");
+        newCompensation = compensationService.create(newCompensation);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCompensationURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newCompensation.getEmployee())
+                .toUri();
+        responseHeaders.setLocation(newCompensationURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 
-    @GetMapping("/compensation/{id}")
-    public Compensation read(@PathVariable String id) {
-        return compensationService.read(id);
+    @GetMapping(value = "/compensation/{id}", produces = "application/json")
+    public ResponseEntity<?> read(@PathVariable String id) {
+        Compensation comp = compensationService.read(id);
+
+        return new ResponseEntity<>(comp, HttpStatus.OK);
     }
 }
